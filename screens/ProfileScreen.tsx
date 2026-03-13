3fafa2db5d45e9087acf0828d9c01407b160dafe
AaPee04 <arttupekka.konttila@gmail.com>
2026-03-13 21:38:33 +0200

    TextInput pohjat

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { View, Text, StyleSheet, Linking, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, Linking, TouchableOpacity, TextInput } from "react-native";
 import { useAuth } from "../src/hooks/useAuth";
 
-export default function ProfileScreen(
-) {
+export default function ProfileScreen() {
 
     const { user } = useAuth();
 
@@ -18,20 +17,42 @@ export default function ProfileScreen(
             </Text>
 
             <View style={styles.profileImage}>
+                <Text style={{ color: "#009FE3", fontSize: 32 }}>+</Text>
             </View>
 
+            <View style={styles.userInfo}>
+                <Text>Email: {user?.email || "N/A"}</Text>
+            </View>
+
+            <View style={styles.userInfo}>
+                <Text>Perussairaudet:</Text>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Lisää perussairaudet"
+                />
+            </View>
+
+            <View style={styles.userInfo}>
+                <Text>Allergiat:</Text>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Lisää allergiat"
+                />
+            </View>
+
+            <Text style={styles.userInfo}>Linkit:</Text>
 
             <View style={styles.row}>
                 <TouchableOpacity onPress={() => openLink("https://www.diabetes.fi/")}>
-                    <Text style={styles.button}>Diabetesliitto </Text>
+                    <Text style={styles.button}>Diabetesliitto</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity onPress={() => openLink("https://www.kanta.fi/omakanta")}>
-                    <Text style={styles.button}>omaKanta </Text>
+                    <Text style={styles.button}>omaKanta</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity onPress={() => openLink("https://www.terveyskyla.fi/diabetestalo")}>
-                    <Text style={styles.button}>Diabetes Talo </Text>
+                    <Text style={styles.button}>Diabetes Talo</Text>
                 </TouchableOpacity>
             </View>
         </View>
@@ -47,17 +68,36 @@ const styles = StyleSheet.create({
     },
     profileName: {
         position: "absolute",
-        top: 40,
-        left: 20,
+        top: 80,
+        left: 25,
         fontSize: 24,
         fontWeight: "bold",
     },
-
     profileImage: {
+        position: "absolute",
+        top: 40,
+        right: 25,
         width: 120,
         height: 120,
-        borderRadius: 60,
-        marginBottom: 20,
+        borderRadius: 30,
+        backgroundColor: "#fff",
+        borderWidth: 2,
+        borderColor: "#009FE3",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    userInfo: {
+        marginTop: 20,
+        fontSize: 18,
+        width: "80%",
+    },
+    textInput: {
+        marginTop: 6,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderRadius: 8,
+        backgroundColor: "#fff",
     },
     row: {
         position: "absolute",
@@ -67,7 +107,6 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "center",
     },
-
     button: {
         backgroundColor: "#009FE3",
         padding: 12,
