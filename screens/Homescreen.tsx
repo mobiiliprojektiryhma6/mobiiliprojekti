a2523843fcf25bca573f12c87ad30e6d8a36b47c
tecnopistacho <pilar.bl.mp@gmail.com>
2026-03-13 10:50:14 +0200

    Profile + Barcode reader

--- screens/Homescreen.tsx
+++ screens/Homescreen.tsx
@@ -23,6 +23,10 @@ export default function Homescreen({ navigation }: { navigation: any }) {
         onPress={() => navigation.navigate("Scanner")}
       >
         <Text style={styles.buttonText}>Open Barcode Scanner</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
         onPress={() => navigation.navigate("Profile")}
       >
         <Text style={styles.buttonText}>Go to Profile</Text>
