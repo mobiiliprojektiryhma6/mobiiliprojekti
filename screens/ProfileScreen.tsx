8906c233402290cca7838017c73419bee8d6295e
AaPee04 <arttupekka.konttila@gmail.com>
2026-03-14 19:51:31 +0200

    ProfileScreen alustava versio valmis

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,8 +1,21 @@
-import React from "react";
-import { View, Text, StyleSheet, Linking, TouchableOpacity, TextInput } from "react-native";
-import { useAuth } from "../src/hooks/useAuth";
+import React, { useState, useEffect } from "react"
+import { View, Text, StyleSheet, Linking, TouchableOpacity, TextInput, Keyboard, Image, Alert } from "react-native"
+import { useCameraPermissions } from 'expo-camera'
+import * as ImagePicker from "expo-image-picker"
+import { useAuth } from "../src/hooks/useAuth"
 
 export default function ProfileScreen() {
+
+    const [weightInput, setWeightInput] = useState("")
+    const [weights, setWeights] = useState<string[]>([])
+    const [heightInput, setHeightInput] = useState("")
+    const [heights, setHeights] = useState<string[]>([])
+    const [diseaseInput, setDiseaseInput] = useState("")
+    const [diseases, setDiseases] = useState<string[]>([])
+    const [allergyInput, setAllergyInput] = useState("")
+    const [allergies, setAllergies] = useState<string[]>([])
+    const [profileImage, setProfileImage] = useState<string | null>(null)
+    const [permission, requestPermission] = useCameraPermissions()
 
     const { user } = useAuth();
 
@@ -10,53 +23,233 @@ export default function ProfileScreen() {
         Linking.openURL(url);
     }
 
+    useEffect(() => {
+        if (!permission) {
+            requestPermission();
+        }
+    }, [permission]);
+
+    if (!permission) {
+        return <Text>Requesting camera permission...</Text>;
+    }
+    if (!permission.granted) {
+        return <Text>No access to camera</Text>;
+    }
+
+
+
+    const addDisease = () => {
+        if (diseaseInput.trim() !== "") {
+            setDiseases([...diseases, diseaseInput]);
+            setDiseaseInput("");
+            Keyboard.dismiss();
+        }
+    }
+
+    const addAllergy = () => {
+        if (allergyInput.trim() !== "") {
+            setAllergies([...allergies, allergyInput]);
+            setAllergyInput("");
+            Keyboard.dismiss();
+        }
+    }
+
+    const addWeight = () => {
+        if (weightInput.trim() !== "") {
+            setWeights([...weights, weightInput]);
+            setWeightInput("");
+            Keyboard.dismiss();
+        }
+    }
+
+    const addHeight = () => {
+        if (heightInput.trim() !== "") {
+            setHeights([...heights, heightInput]);
+            setHeightInput("");
+            Keyboard.dismiss();
+        }
+    }
+
+    const pickFromLibrary = async () => {
+        const permission = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (!permission.granted) {
+            alert("Salli kuvien käyttö asetuksista.");
+            return
+        }
+
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setProfileImage(result.assets[0].uri);
+        }
+    }
+
+    const takePhoto = async () => {
+        const permission = await ImagePicker.requestCameraPermissionsAsync();
+        if (!permission.granted) {
+            alert("Salli kameran käyttö asetuksista.");
+            return
+        }
+
+        const result = await ImagePicker.launchCameraAsync({
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        })
+
+        if (!result.canceled) {
+            setProfileImage(result.assets[0].uri);
+        }
+    }
+
+    const chooseImageOption = () => {
+        Alert.alert(
+            "Valitse vaihtoehto",
+            "",
+            [
+                { text: "Ota kuva", onPress: takePhoto },
+                { text: "Valitse kirjastosta", onPress: pickFromLibrary },
+                { text: "Peruuta", style: "cancel" }
+            ]
+        )
+    }
+
+
     return (
         <View style={styles.container}>
-            <Text style={styles.profileName}>
-                {user?.displayName ? `${user.displayName}'s Profile` : "Profile Screen"}
-            </Text>
 
-            <View style={styles.profileImage}>
-                <Text style={{ color: "#009FE3", fontSize: 32 }}>+</Text>
+            <View style={styles.headerLeft}>
+                <Text style={styles.profileName}>
+                    {user?.displayName || "Profile Screen"}
+                </Text>
+                <Text style={styles.emailName}>
+                    {user?.email || "N/A"}
+                </Text>
             </View>
 
-            <View style={styles.userInfo}>
-                <Text>Email: {user?.email || "N/A"}</Text>
+            <TouchableOpacity style={styles.profileImage} onPress={chooseImageOption}>
+                {profileImage ? (
+                    <Image
+                        source={{ uri: profileImage }}
+                        style={{ width: "100%", height: "100%", borderRadius: 30 }}
+                    />
+                ) : (
+                    <Text style={{ color: "#009FE3", fontSize: 32 }}>+</Text>
+                )}
+            </TouchableOpacity>
+
+            <View style={styles.section}>
+                <Text style={styles.sectionTitle}>Perustiedot:</Text>
+
+                <View style={styles.rowBetween}>
+
+                    <View style={styles.column}>
+                        <Text>Pituus (cm):</Text>
+                        <View style={styles.rowCenter}>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Lisää pituus"
+                                value={heightInput}
+                                onChangeText={setHeightInput}
+                                keyboardType="numeric"
+                            />
+                            <TouchableOpacity style={styles.smallButton} onPress={addHeight}>
+                                <Text style={styles.smallButtonText}>+</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        {heights.map((item, index) => (
+                            <Text key={index} style={styles.listItem}>• {item} cm</Text>
+                        ))}
+                    </View>
+
+                    <View style={styles.column}>
+                        <Text>Paino (kg):</Text>
+                        <View style={styles.rowCenter}>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Lisää paino"
+                                value={weightInput}
+                                onChangeText={setWeightInput}
+                                keyboardType="numeric"
+                            />
+                            <TouchableOpacity style={styles.smallButton} onPress={addWeight}>
+                                <Text style={styles.smallButtonText}>+</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        {weights.map((item, index) => (
+                            <Text key={index} style={styles.listItem}>• {item} kg</Text>
+                        ))}
+                    </View>
+
+                </View>
             </View>
 
-            <View style={styles.userInfo}>
-                <Text>Perussairaudet:</Text>
-                <TextInput
-                    style={styles.textInput}
-                    placeholder="Lisää perussairaudet"
-                />
+            <View style={styles.section}>
+                <Text style={styles.sectionTitle}>Perussairaudet:</Text>
+
+                <View style={styles.rowCenter}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Lisää perussairaus"
+                        value={diseaseInput}
+                        onChangeText={setDiseaseInput}
+                    />
+                    <TouchableOpacity style={styles.smallButton} onPress={addDisease}>
+                        <Text style={styles.smallButtonText}>+</Text>
+                    </TouchableOpacity>
+                </View>
+
+                {diseases.map((item, index) => (
+                    <Text key={index} style={styles.listItem}>• {item}</Text>
+                ))}
             </View>
 
-            <View style={styles.userInfo}>
-                <Text>Allergiat:</Text>
-                <TextInput
-                    style={styles.textInput}
-                    placeholder="Lisää allergiat"
-                />
+            <View style={styles.section}>
+                <Text style={styles.sectionTitle}>Allergiat:</Text>
+
+                <View style={styles.rowCenter}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Lisää allergia"
+                        value={allergyInput}
+                        onChangeText={setAllergyInput}
+                    />
+                    <TouchableOpacity style={styles.smallButton} onPress={addAllergy}>
+                        <Text style={styles.smallButtonText}>+</Text>
+                    </TouchableOpacity>
+                </View>
+
+                {allergies.map((item, index) => (
+                    <Text key={index} style={styles.listItem}>• {item}</Text>
+                ))}
             </View>
 
-            <Text style={styles.userInfo}>Linkit:</Text>
+            <Text style={styles.linksTitle}>Linkit:</Text>
 
-            <View style={styles.row}>
+            <View style={styles.rowCenterAbsolute}>
                 <TouchableOpacity onPress={() => openLink("https://www.diabetes.fi/")}>
-                    <Text style={styles.button}>Diabetesliitto</Text>
+                    <Text style={styles.linkButton}>Diabetesliitto</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity onPress={() => openLink("https://www.kanta.fi/omakanta")}>
-                    <Text style={styles.button}>omaKanta</Text>
+                    <Text style={styles.linkButton}>omaKanta</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity onPress={() => openLink("https://www.terveyskyla.fi/diabetestalo")}>
-                    <Text style={styles.button}>Diabetes Talo</Text>
+                    <Text style={styles.linkButton}>Diabetes Talo</Text>
                 </TouchableOpacity>
             </View>
+
         </View>
     )
+
 }
 
 const styles = StyleSheet.create({
@@ -64,15 +257,24 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#E5F7FD"
+        backgroundColor: "#E5F7FD",
     },
-    profileName: {
+
+    headerLeft: {
         position: "absolute",
         top: 80,
-        left: 25,
+        left: 60,
+    },
+    profileName: {
         fontSize: 24,
         fontWeight: "bold",
     },
+    emailName: {
+        fontSize: 16,
+        color: "#555",
+        marginTop: 4,
+    },
+
     profileImage: {
         position: "absolute",
         top: 40,
@@ -86,20 +288,70 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
     },
-    userInfo: {
+
+    section: {
         marginTop: 20,
-        fontSize: 18,
         width: "80%",
     },
-    textInput: {
+    sectionTitle: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 6,
+    },
+
+    rowCenter: {
+        flexDirection: "row",
+        alignItems: "center",
         marginTop: 6,
+    },
+    rowBetween: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "100%",
+    },
+    column: {
+        width: "48%",
+    },
+
+    input: {
+        flex: 1,
         padding: 10,
         borderWidth: 1,
         borderColor: "#ccc",
         borderRadius: 8,
         backgroundColor: "#fff",
     },
-    row: {
+
+    smallButton: {
+        marginLeft: 8,
+        backgroundColor: "#009FE3",
+        width: 40,
+        height: 40,
+        borderRadius: 8,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    smallButtonText: {
+        color: "#fff",
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+
+    listItem: {
+        marginTop: 4,
+        fontSize: 16,
+    },
+
+    linksTitle: {
+        position: "absolute",
+        bottom: 80,
+        left: 0,
+        right: 0,
+        textAlign: "center",
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+    rowCenterAbsolute: {
         position: "absolute",
         bottom: 20,
         left: 0,
@@ -107,7 +359,7 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "center",
     },
-    button: {
+    linkButton: {
         backgroundColor: "#009FE3",
         padding: 12,
         borderRadius: 8,
